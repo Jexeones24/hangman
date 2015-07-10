8a34bdee57aa50b12ef52b41b83cbf4054775098
Andrew Baik <and.baik@gmail.com>
2015-07-10 19:45:42 -0400

    added difficulty selector with its own erb template

--- hangman.rb
+++ hangman.rb
@@ -4,15 +4,19 @@ require 'sinatra/reloader'
 set :secret_number, 1 + rand(99)
 @@bg_color =  "white"
 @@guess_remain = 5
+@@prompt = "Set the difficulty. Enter an integer (between 1 and 29, inclusive) to set number of letters in target word:"
+@@difficulty = nil
+dictionary = File.open("enable.txt", "r") { |file| file.readlines }
 
 get '/' do
+  @@difficulty = params["difficulty"] if !@@difficulty
   cheat_condition = params["check"] == true && params["check"] != ""
   if params["guess"] && params["guess"] != "" && !params["check"]
     message = check_guess(params["guess"])
     @@guess_remain -= 1
     color = @@bg_color
-  elsif !params["guess"] || params["guess"] == ""
-    message = "Guess a number!"
+  elsif !params["guess"] || params["guess"] == "" || params["guess"].length > 1
+    message = "Guess a letter!"
   end
 
   got_answer = params["guess"].to_i == settings.secret_number
@@ -28,7 +32,12 @@ get '/' do
   sn = settings.secret_number
 
   message += " CHEAT SOLUTION: #{settings.secret_number}" if params["cheat"]
-  erb :index, :locals => {:message => message, :color => color, :sn => sn }
+  init_prompt = @@prompt
+  if @@difficulty && @@difficulty != ""
+    erb :play, :locals => { :message => message, :color => color, :sn => sn }
+  else
+    erb :index, :locals => {:init_prompt => init_prompt}
+  end
 end
 
 def check_guess(guess)
