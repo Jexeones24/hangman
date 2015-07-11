6d8a37d72c324f02ce69d771a69fe674c4c88260
Andrew Baik <and.baik@gmail.com>
2015-07-11 03:00:27 -0400

    added win/loss conditions with restart option

--- hangman.rb
+++ hangman.rb
@@ -2,15 +2,14 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 #initialization
-set :secret_number, 1 + rand(99)
 set :prompt, "Set the difficulty. Enter an integer (between 2 and 28, "\
 "inclusive) to set number of letters in target word:"
 set :all_letters, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 set :all_difficulty, (2..28)
 
+set :dictionary, File.open("enable.txt", "r") { |file| file.readlines }
 set :bg_color, "FFFFFF"
 set :difficulty, nil
-set :dictionary, File.open("enable.txt", "r") { |file| file.readlines }
 set :word, ""
 set :state, []
 set :guess_remain, 5
@@ -18,8 +17,13 @@ set :proper_guess, false
 set :correct_guess, false
 set :bad_guess, Hash.new{0}
 set :proper_difficulty, false
+set :cheat_condition, false
 
 get '/' do
+  if params["guess"].eql? "restart"
+    reset_everything
+  end
+  #setting local variables and etc.
   error_difficulty = ""
   error_guess = ""
   correct_msg = ""
@@ -33,7 +37,6 @@ get '/' do
         settings.difficulty = params["difficulty"].to_i
         find_word(params["difficulty"].to_i)
         settings.proper_difficulty = true
-        #settings.bad_guess.clear
       else
         error_difficulty = "Error: Input not valid! Please enter an integer"\
         " between 2 and 28, inclusive!"
@@ -43,6 +46,7 @@ get '/' do
 
   #check for proper user guess inputs (must be a single letter)
   not_empty_guess = params["guess"] && params["guess"] != ""
+  settings.proper_guess = false
   if not_empty_guess
     if (settings.all_letters.include? params["guess"]) &&
       (params["guess"].length == 1)
@@ -53,44 +57,41 @@ get '/' do
   end
 
   #check for cheat mode
-  cheat_condition = params["cheat"].eql? "true"
+  settings.cheat_condition = params["cheat"].eql? "true"
 
   #Process Guesses
-  if settings.proper_guess
-    message = check_guess(params["guess"])
-    if settings.correct_guess
-      correct_msg += "Your guess, #{params["guess"]}, was correct!"
+  if settings.guess_remain > 0 && !win_checker
+    if settings.proper_guess
+      check_guess(params["guess"])
+      if settings.correct_guess
+        correct_msg += "Your guess, #{params["guess"]}, was correct!"
+        settings.bg_color = "00FF00"
+      else
+        correct_msg += "#{params["guess"]} is incorrect..."
+        settings.bg_color = "FF0000"
+      end
+      color = settings.bg_color
+      guess_msg += settings.guess_remain.to_s
     else
-      correct_msg += "#{params["guess"]} is incorrect..."
+      error_guess = "Guess a single letter!"\
+      " Your guess won't count otherwise!"
+      guess_msg += settings.guess_remain.to_s
     end
-    color = settings.bg_color
-    guess_msg += settings.guess_remain.to_s
-  else
-    error_guess = "Guess a single letter!"\
-    " Your guess won't count otherwise!"
-    guess_msg += settings.guess_remain.to_s
+  elsif settings.guess_remain <= 0
+    guess_msg += "0"
+    correct_msg = "You've lost your 5 guesses! The word was: #{settings.word}."\
+    " Type \"restart\" and click the \"Guess Letter\" button to restart!"
   end
-
-  #Process game ending (win or lose)
-  got_answer = params["guess"].to_i == settings.secret_number
-  if settings.guess_remain == 0 && !got_answer
-    settings.secret_number = 1 + rand(99)
-    settings.guess_remain = 5
-    message = "You've lost your 5 guesses! A new number has been generated!"
-  elsif got_answer
-    settings.secret_number = 1 + rand(99)
-    settings.guess_remain = 5
-    message = "You've guessed correctly! A new number has been generated!"
+  if win_checker
+    correct_msg = "You won!!!"\
+    " Type \"restart\" and click the \"Guess Letter\" button to restart!"
   end
-  sn = settings.secret_number
 
   #Rendering proper ERB templates
-  message += " CHEAT SOLUTION: #{settings.secret_number}" if cheat_condition
+  message += " CHEAT SOLUTION: #{settings.word}" if settings.cheat_condition
   init_prompt = settings.prompt
   progress = display_progress
   bad_guess_msg = "Incorrect guesses: " + bad_guess_tally
-  answer = settings.word + ((settings.word).length.to_s) + " "
-  answer += settings.difficulty.to_s
 
   if settings.proper_difficulty
     erb :play, :locals => {
@@ -99,7 +100,6 @@ get '/' do
       :progress => progress,
       :error_guess => error_guess,
       :guess_msg => guess_msg,
-      :answer => answer,
       :correct_msg => correct_msg,
       :bad_guess_msg => bad_guess_msg
     }
@@ -114,8 +114,6 @@ end
 
 #finds random word of proper length at app level
 def find_word(difficulty)
-  settings.word = ""
-  settings.state = []
   #NEWLINE COUNTS AS A CHARACTER UGHHHH
   possible_words = settings.dictionary.select { |entry| entry.length-1 == difficulty}
   settings.word = possible_words[rand(possible_words.length)][0...-1]
@@ -148,6 +146,7 @@ def check_guess(guess)
     end
   end
   if !settings.correct_guess
+    settings.guess_remain -= 1
     settings.bad_guess[guess] += 1
   end
 end
@@ -155,4 +154,29 @@ end
 #returns a string of all bad guesses
 def bad_guess_tally
   return settings.bad_guess.keys.to_s
+end
+
+#returns true if answer is obtained, otherwise false
+def win_checker
+  settings.state.each do |element|
+    return false if element == false
+  end
+  if settings.guess_remain >= 0
+    return true
+  end
+  return false
+end
+
+#reset all game settings to initial settings
+def reset_everything
+  settings.bg_color = "FFFFFF"
+  settings.difficulty = nil
+  settings.word = ""
+  settings.state.clear
+  settings.guess_remain = 5
+  settings.proper_guess = false
+  settings.correct_guess = false
+  settings.bad_guess.clear
+  settings.proper_difficulty = false
+  settings.restart = ""
 end
